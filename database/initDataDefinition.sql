7d06adae2a03847a898f5b1710663fedce7e4db3
Katarzhin Mikhail <maikl.1997@mail.ru>
2023-12-02 13:48:57 +0400

    [fix] set track_number as FK from  items to orders; set transaction as FK from  payment to orders.order_uid

--- database/initDataDefinition.sql
+++ database/initDataDefinition.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS orders (
-    order_uid VARCHAR(50) PRIMARY KEY,
+    order_uid VARCHAR(50),
     track_number VARCHAR(50),
     entry VARCHAR(5),
     locale VARCHAR(10),
@@ -9,7 +9,9 @@ CREATE TABLE IF NOT EXISTS orders (
     shardkey VARCHAR(50),
     sm_id INTEGER,
     date_created TIMESTAMP,
-    oof_shard VARCHAR(10)
+    oof_shard VARCHAR(10),
+    CONSTRAINT pk_orders PRIMARY KEY (order_uid),
+    CONSTRAINT uq_track_number UNIQUE (track_number)
     );
 
 CREATE TABLE IF NOT EXISTS delivery(
@@ -26,7 +28,6 @@ CREATE TABLE IF NOT EXISTS delivery(
     );
 
 CREATE TABLE IF NOT EXISTS payment (
-    order_uid VARCHAR(50),
     transaction VARCHAR(50),
     request_id VARCHAR(50),
     currency VARCHAR(3),
@@ -38,8 +39,7 @@ CREATE TABLE IF NOT EXISTS payment (
     goods_total INTEGER,
     custom_fee INTEGER,
     CONSTRAINT pk_payment PRIMARY KEY (transaction),
-    CONSTRAINT uq_order_uid UNIQUE (order_uid),
-    CONSTRAINT fk_payment_belongs_to_order FOREIGN KEY (order_uid) REFERENCES orders(order_uid),
+    CONSTRAINT fk_payment_belongs_to_order FOREIGN KEY (transaction) REFERENCES orders(order_uid),
     CONSTRAINT ch_amount_not_negative CHECK ( amount >= 0 ),
     CONSTRAINT ch_delivery_cost_not_negative CHECK ( delivery_cost >= 0 ),
     CONSTRAINT ch_goods_total_not_negative CHECK ( goods_total >= 0 ),
@@ -47,7 +47,6 @@ CREATE TABLE IF NOT EXISTS payment (
     );
 
 CREATE TABLE IF NOT EXISTS items (
-    order_uid VARCHAR(50),
     chrt_id INTEGER,
     track_number VARCHAR(50),
     price INTEGER,
@@ -60,7 +59,7 @@ CREATE TABLE IF NOT EXISTS items (
     brand VARCHAR(50),
     status INTEGER,
     CONSTRAINT pk_items PRIMARY KEY (rid),
-    CONSTRAINT fk_items_belongs_to_order FOREIGN KEY (order_uid) REFERENCES orders(order_uid),
+    CONSTRAINT fk_items_belongs_to_order FOREIGN KEY (track_number) REFERENCES orders(track_number),
     CONSTRAINT ch_price_not_negative CHECK ( price >= 0 ),
     CONSTRAINT ch_sale_not_negative CHECK ( sale >= 0 ),
     CONSTRAINT ch_total_price_not_negative CHECK ( price >= 0 )
